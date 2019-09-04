79d4676cfd803d0e6a4c7185a538b326a0885bbd
TheFloatingSheep <georgebaran1999@gmail.com>
2019-09-04 20:05:18 +0100

    Progress on C# WebSocket Implem.

--- Assets/WebSocket.cs
+++ Assets/WebSocket.cs
@@ -2,11 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Net.WebSockets;
+using System;
+using System.Threading;
+using System.Text;
 
 public class WebSocket : MonoBehaviour
 {
+    private ClientWebSocket client;
+    public void wsConnect(string ip)
+    {
+        client = new ClientWebSocket();
+        client.ConnectAsync(new Uri(ip), new CancellationToken());
+    }
+
+    public void wsClose()
+    {
+        client.CloseAsync(new WebSocketCloseStatus(), "", new CancellationToken());
+    }
+
+    public void wsSend(string str)
+    {
+        byte[] strB = Encoding.UTF8.GetBytes(str);
+        ArraySegment<Byte> AS = new ArraySegment<Byte>(strB);
+
+        client.SendAsync(AS, new WebSocketMessageType(), false, new CancellationToken());
+    }
+
+    wsClient clientscript;
     void Start()
     {
-        
+        clientscript = gameObject.GetComponent<wsClient>();
+        Reading();
+    }
+
+    string Data;
+    public IEnumerator Listen()
+    {
+        while (true)
+        {
+            if(client.State == WebSocketState.Open)
+            {
+                clientscript.onOpen();
+            }
+            if (client.State == WebSocketState.Closed)
+            {
+                clientscript.onClose();
+            }
+            if (client.State == WebSocketState.Aborted)
+            {
+                clientscript.onError();
+            }
+        }
+    }
+
+    async void Reading()
+    {
+
     }
 }
--- Assets/wsClient.cs
+++ Assets/wsClient.cs
@@ -14,9 +14,50 @@ public class wsClient : MonoBehaviour
     [DllImport("__Internal")]
     private static extern void wsClose();
 
+    public enum platform {WebGL, Standalone}
+
+    public platform Platform = platform.WebGL;
+
+    WebSocket csws;
     void Start()
     {
-        wsConnect("ws://189.223.237.64:8080");
+        csws = gameObject.GetComponent<WebSocket>();
+    }
+
+    void Connect(string ip)
+    {
+        if(Platform == platform.WebGL)
+        {
+            wsConnect("ws://189.223.237.64:8080");
+        }
+        else if(Platform == platform.Standalone)
+        {
+            csws.wsConnect(ip);
+        }
+    }
+
+    void Send(string str)
+    {
+        if (Platform == platform.WebGL)
+        {
+            wsSend(str);
+        }
+        else if (Platform == platform.Standalone)
+        {
+            
+        }
+    }
+
+    void Close()
+    {
+        if (Platform == platform.WebGL)
+        {
+            wsClose();
+        }
+        else if (Platform == platform.Standalone)
+        {
+            csws.wsClose();
+        }
     }
 
     public void onOpen()
