845de8c5ae91de9f7da2400537d7856eb0c20344
Andr3wRiv3rs <andrewarivers@gmail.com>
2019-09-05 15:06:12 -0700

    Unzip jslib & Template

---
+++ Assets/Plugins/unzip.jslib
@@ -0,0 +1,7 @@
+mergeInto(LibraryManager.library, {
+    UnzipFilePrompt: function () {
+        window.UnzipFilePrompt();
+        console.log('Setup Unzip');
+    },
+})
+    
---
+++ Assets/Unzip.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class Unzip : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void UnzipFilePrompt();
+
+    void Start()
+    {
+        UnzipFilePrompt();
+    }
+}
