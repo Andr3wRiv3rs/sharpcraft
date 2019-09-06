e57746984fc05b3430d8f57b41db9264472db47c
TheFloatingSheep <georgebaran1999@gmail.com>
2019-09-06 01:18:33 +0100

    Added Mono.nat and struggle on events continues

--- Assets/WebSocket.cs
+++ Assets/WebSocket.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DysnomiaWebSocketClient;
 
 public class WebSocket : MonoBehaviour
 {
     DysnomiaWebSocketClient.ClientWS client;
     private void Start()
     {
-        client = new DysnomiaWebSocketClient.ClientWS();
+        client = new ClientWS();
         client.Connect("ws://shielded-plateau-64817.herokuapp.com/");
     }
 
@@ -19,4 +20,10 @@ public class WebSocket : MonoBehaviour
             Debug.Log("ok");
         }
     }
+
+    public void onOpen(object sender, wsEventArgs eventargs)
+    {
+
+    }
+
 }
