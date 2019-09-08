19c416c69f75f44f9fe256451d749f507ac1ac77
TheFloatingSheep <georgebaran1999@gmail.com>
2019-09-08 15:20:33 +0100

    Rudimentary Player Movement and Mouselook rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,14 +7,31 @@ public class Player : MonoBehaviour
     public float ForwardSpeed = 1;
     public float SideSpeed = 1;
     public float BackSpeed = 1;
-    
+    public float SprintingSpeed = 1;
+    public float JumpHeight = 1;
+
+    private float Forward_Speed;
+    private float Side_Speed;
+    private float Back_Speed;
+    private float Sprinting_Speed;
+    private float Jump_Height;
+
+    private void Start()
+    {
+        Forward_Speed = ForwardSpeed;
+        Side_Speed = SideSpeed;
+        Back_Speed = BackSpeed;
+        Sprinting_Speed = SprintingSpeed;
+        Jump_Height = JumpHeight;
+    }
+
     private void FixedUpdate()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            gameObject.GetComponent<MouseLook>().enabled = true;
+            //Cursor.lockState = CursorLockMode.Locked;
+            //Cursor.visible = false;
+            gameObject.transform.GetChild(0).GetComponent<MouseLook>().enabled = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -25,24 +42,28 @@ public class Player : MonoBehaviour
         }
 
         Vector3 newPosition = transform.position;
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
         if (Input.GetKey(KeyCode.W))
         {
-            newPosition += transform.forward * ForwardSpeed;
+            newPosition += transform.forward * (Forward_Speed / 50);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            newPosition -= transform.forward * BackSpeed;
+            newPosition -= transform.forward * (Back_Speed / 50);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            newPosition += transform.right * SideSpeed;
+            newPosition += transform.right * (Side_Speed / 50);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            newPosition -= transform.right * SideSpeed;
+            newPosition -= transform.right * (Side_Speed / 50);
         }
 
-        newPosition.y = 0;
+        
+
+        newPosition.y = transform.position.y;
 
         transform.position = newPosition;
     }
