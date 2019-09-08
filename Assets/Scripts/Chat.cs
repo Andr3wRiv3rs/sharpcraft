954e3644ab2873d2b3a7e19cff2bc07eefad2b06
TheFloatingSheep <georgebaran1999@gmail.com>
2019-09-08 16:22:31 +0100

    Chat and Server Master Progress

---
+++ Assets/Scripts/Chat.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chat : MonoBehaviour
+{
+    public wsClient WSCLIENT;
+
+    public List<GameObject> Bubbles;
+
+    public InputField inputField;
+
+    public int MessageLimit = 10;
+
+    private void Awake()
+    {
+        HideInput();
+    }
+
+    private void Start()
+    {
+        HideInput();
+    }
+
+    public void HideInput()
+    {
+        inputField.gameObject.SetActive(false);
+        //make all bubbles lighter
+    }
+
+    public void ShowInput()
+    {
+        inputField.gameObject.SetActive(true);
+        //make all bubbles darker
+    }
+
+    public void SendChat(string Message)
+    {
+        WSCLIENT.SendWS(Message);
+    }
+
+    string[] Split(string str)
+    {
+        return str.Split(new char[] { 'γ' });
+    }
+
+    bool isChat(string str)
+    {
+        if (Split(str)[0] == "MSG")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void onMessage(string str)
+    {
+        if (isChat(str))
+        {
+            NewBubble(str);
+
+            if (Bubbles.Count > MessageLimit)
+            {
+                Bubbles.Remove(Bubbles[Bubbles.Count - 1]);
+            }
+        }
+    }
+
+    public void NewBubble(string str)
+    {
+        //add new bubble to list
+    }
+
+    public void DisplayBubbles()
+    {
+        //display all bubbles out of Bubbles list
+    }
+
+    bool FieldSwitch = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (FieldSwitch)
+            {
+                FieldSwitch = false;
+                HideInput();
+            }
+            else
+            {
+                FieldSwitch = true;
+                ShowInput();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ServerMaster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerMaster : MonoBehaviour
+{
+    public wsClient WSCLIENT;
+    public GameObject Chat;
+
+    void Connect(string ip)
+    {
+        WSCLIENT.ConnectWS(ip);
+    }
+
+    public void onOpen()
+    {
+
+    }
+
+    public void onClose()
+    {
+        //show disconnected page
+        ShowChat(false);
+    }
+
+    void ClearChat()
+    {
+        for(int i = 0; i < GameObject.FindGameObjectsWithTag("Bubble").Length; i++)
+        {
+            Destroy(GameObject.FindGameObjectsWithTag("Bubble")[i]);
+        }
+        GameObject.FindGameObjectWithTag("InputField").GetComponent<InputField>().text = "";
+    }
+
+    void ShowChat(bool Fact)
+    {
+        Chat.GetComponent<Chat>().HideInput();
+        Chat.SetActive(Fact);
+    }
+}
